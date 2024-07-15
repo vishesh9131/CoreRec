28d1c8bc6ae7f515d91503b35f49a4927624a255
Vishesh <87526302+vishesh9131@users.noreply.github.com>
2024-07-15 14:50:02 +0530

    structured

--- .history/SANDBOX/nidar_20240708011127.py
+++
@@ -1,9 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore")
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-import warnings
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    # Your code here
