1b0f6320e314847eb832c19a1d24ad0509e3fca2
Vishesh <87526302+vishesh9131@users.noreply.github.com>
2024-07-15 15:31:24 +0530

    unittest fix

--- src/USECASES/unittest/custommodel.py
+++ src/USECASES/unittest/custommodel.py
@@ -3,7 +3,7 @@ import os
 import unittest
 
 # Add the parent directory to the system path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 # Now you can import the modules from the parent directory
 import core_rec as cs
