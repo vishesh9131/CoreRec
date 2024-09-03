3812aee14dd161ee9f2550e65cc8c643be2aa1aa
Vishesh <87526302+vishesh9131@users.noreply.github.com>
2024-09-03 14:56:29 +0530

    README Fix
    1. Added a new TaseCase

--- src/USECASES/tempCodeRunnerFile.py
+++ src/USECASES/tempCodeRunnerFile.py
@@ -1,12 +1,6 @@
-top_nodes = vg.find_top_nodes(adj_matrix, num_nodes=5)
-
-    # Train the model
-num_epochs = 10
-cs.train_model(model, data_loader, criterion, optimizer, num_epochs)
-
-
-    # Predict recommendations for a specific node
-node_index = 2   #target node
-recommended_nodes = cs.predict(model, adj_matrix, node_index, top_k=5, threshold=0.5)
-print(f"Recommended nodes for node {node_index}: {recommended_nodes}")
-
+graph = [
+#     [0, 10, 15, 20],
+#     [5, 0, 9 , 10],
+#     [6, 13, 0, 12],
+#     [8, 8, 9, 0]
+# ]
