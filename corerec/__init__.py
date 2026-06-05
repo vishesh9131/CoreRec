7824e9bd80f119049173b952d2faccc631c87219
vishesh9131 <sciencely98@gmail.com>
2026-06-05 16:16:14 +0530

    release: bump version to 0.5.4

--- corerec/__init__.py
+++ corerec/__init__.py
@@ -20,7 +20,7 @@ Author: Vishesh Yadav (sciencely98@gmail.com)
 License: MIT (see pyproject.toml)
 """
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 __author__ = "Vishesh Yadav"
 __email__ = "sciencely98@gmail.com"
 
